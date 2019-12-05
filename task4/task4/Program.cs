a70c71a4486b3a54d8749bbb5b11457a72324532
Vadim Kolodko <kolodko98@mail.ru>
2019-12-05 14:38:28 +0300

    Add task 4 and task 5

---
+++ task4/task4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listWithNumber = new List<int>() { 5 };
+            Console.WriteLine(Modificate(listWithNumber, (a) => a[0] * a[0]));
+            Modificate(listWithNumber, (a) => a[0] * a[0] * a[0]);
+            Modificate(listWithNumber, (a) => -a[0]);
+
+            List<int> listOfNumbers = new List<int>() { 1, 534, 12, 7, 4, 0, 456 };
+            foreach (var element in Filter<int>(listOfNumbers, (x) => x > 13).ToList())
+                Console.Write(element + " ");
+
+            Console.WriteLine("\n" + Compose<int>(10, (x) => x * x, (x) => x * x * x));
+        }
+
+        private static int Modificate(List<int> argument, Func<List<int>, int> function)
+        {
+            return function(argument);
+        }
+
+        private static IEnumerable<T> Filter<T>(IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            return collection.Where(x => predicate(x));
+        }
+
+        private static T Compose<T>(T argument, Func<T, T> firstFunction, Func<T, T> secondFunction)
+        {
+            return secondFunction(firstFunction(argument));
+        }
+    }
+}
---
+++ task5/task5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var element in
+                Console.ReadLine().Split(' ').Select(Int32.Parse).ToList()
+                .Select((x, i) => new { x, i })
+                .ToDictionary(x => x.i, x => x.x)
+                .OrderBy((x) => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value)
+                .Zip(Console.ReadLine().Split(' ').Select(Int32.Parse).ToList().Select((x, i) => new { x, i })
+                .ToDictionary(x => x.i, x => x.x)
+                .OrderBy((x) => x.Value)
+                .Reverse()
+                .ToDictionary(x => x.Key, x => x.Value), (pas, tax) => new KeyValuePair<int, int>(pas.Key, tax.Key))
+                .OrderBy((x) => x.Key))
+            {
+                Console.Write(element.Value + " ");
+            }
+
+
+            string input = Console.ReadLine();
+            var punctuation = input.Where(Char.IsPunctuation).Distinct().ToArray();
+            var arrayOfWords = input.Split().Select(x => x.Trim(punctuation)).Where(x => x!="");
+            var groupedWords = arrayOfWords.GroupBy(x => x);
+            var output = groupedWords.Select(x => new KeyValuePair<string, int>(x.Key, x.Count()));
+            foreach(var element in Console.ReadLine()
+                .Split().Select(x => x.Trim(
+                    input.Where(Char.IsPunctuation).Distinct().ToArray())).Where(x => x != "")
+                .GroupBy(x => x)
+                .Select(x => new KeyValuePair<string, int>(x.Key, x.Count())))
+                Console.WriteLine(element);
+        }
+    }
+}
